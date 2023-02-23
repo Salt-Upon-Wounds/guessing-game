597a105977dcea5e5b3a2bd76e0de0f1556fc711
Salt-Upon-Wounds <kiw197204@gmail.com>
2023-02-23 16:46:45 +0300

    asd

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
+    min = 0;
+    max = 0;
+    lastGuess = 0;
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.lastGuess = Math.ceil((this.max + this.min) / 2);
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.max = this.lastGuess
     }
 
     greater() {
-
+        this.min = this.lastGuess
     }
 }
 
